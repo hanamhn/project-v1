f05854e36e13b10eb0a1c394783b544cbbe96353
hanamhn <hanamhndev@gmail.com>
2021-01-28 11:45:31 +0700

    recoil

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import React from "react";
+import { atom, useRecoilState, useRecoilValue, selector } from "recoil";
+
+const x2Selector = selector({
+  key: "x2Selector",
+  get: ({ get }) => {
+    const count = get(counterState);
+
+    return count * 2;
+  },
+});
+
+const counterState = atom({
+  key: "counterState",
+  default: 0,
+});
 
 const App = () => {
+  const [count, setCount] = useRecoilState(counterState);
+  const doubleCount = useRecoilValue(x2Selector);
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
   return (
     <div>
-      <h1>Hello</h1>
+      <p>Count: {count}</p>
+      <p>Double Count: {doubleCount}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
     </div>
   );
 };
